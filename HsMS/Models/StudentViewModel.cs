6400e9fb29e6c64609c103a7039ee806b3e03ba5
Sheikh Abdul Kaiyum <46687618+skaiyum5@users.noreply.github.com>
2021-08-10 16:29:52 +0600

    Add files via upload

---
+++ HsMS/Models/StudentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HsMS.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string AdmissionDate { get; set; }
+        public int ClassId { get; set; }
+        public List<StudentClass> StudentClasses { get; set; }
+        public StudentViewModel()
+        {
+            StudentClasses = new List<StudentClass>();
+        }
+    }
+}
